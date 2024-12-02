113c40312c141ec8e6bcbf1acbf0d70e44181d5c
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2024-12-02 23:04:35 +0530

    Fix php 8.3 support

--- inc/Utils.php
+++ inc/Utils.php
@@ -711,7 +711,7 @@ class Utils extends Base {
 		$library_popup_style = $options->get( 'library_popup_style' );
 
 		$css .= ".elementor-add-template-button {
-					display: ${hide_elementor_library};
+					display: {$hide_elementor_library};
 				}";
 
 		if ( isset( $library_popup_style ) && 'full-screen' === $library_popup_style ) {
