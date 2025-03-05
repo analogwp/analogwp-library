edd14584227eb540784b8add9fdd73f2e3cd62ff
Krishna Kant Chourasiya <lushkant@users.noreply.github.com>
2025-03-06 00:03:49 +0530

    Add Library text script global

--- inc/class-elementor.php
+++ inc/class-elementor.php
@@ -79,7 +79,7 @@ class Elementor {
 			'analog/library/app/strings',
 			array(
 				'is_settings_page'   => false,
-				'library_title_text' => $options->has( 'library_title_text' ) ? $options->get( 'library_title_text' ) : __( 'Library', 'analogwp-library-pro' ),
+				'library_title_text' => Plugin::instance()->has_pro_active() && $options->has( 'library_title_text' ) ? $options->get( 'library_title_text' ) : __( 'Library', 'analogwp-library' ),
 			)
 		);
 
